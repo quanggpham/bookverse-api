4c5362ad0e935c292f70e8d4bcd402584feb7ecc
quangpham <phamquangdung188@gmail.com>
2026-08-04 19:08:07 +0700

    feat: add Spring Boot main application class

---
+++ src/main/java/com/internship/bookverse/BookVerseApplication.java
@@ -0,0 +1,12 @@
+package com.internship.bookverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookVerseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookVerseApplication.class, args);
+    }
+}
